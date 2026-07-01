4201d6c5c5bc7c6149742b300c688ad0fb815a74
Cameron Beesley <59356485+funnyfriend77@users.noreply.github.com>
2026-07-01 17:27:01 -0600

    Added a page that shows all the available groups a user can join

--- sailor_church/connect/views.py
+++ sailor_church/connect/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from pages.models import ConnectGroup
 
 # Create your views here.
 def index(request):
     return render(request, 'connect/index.html', {
         'groups': request.user.joined_groups.all()
+    })
+
+def join(request):
+    return render(request, 'connect/join_page.html', {
+        'available_groups': ConnectGroup.objects.all()
     })
